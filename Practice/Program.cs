586f55af7558581e896ab3aac599ed0814182ea8
olayemi odefunsho <odefunsho.olayemi@gmail.com>
2020-05-17 22:56:51 +0100

    Array Largest Range

--- Practice/Program.cs
+++ Practice/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Practice
 {
@@ -6,7 +8,89 @@ namespace Practice
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] array = new int[] { 19, -1,
+    18,
+    17,
+    2,
+    10,
+    3,
+    12,
+    5,
+    16,
+    4,
+    11,
+    8,
+    7,
+    6,
+    15,
+    12,
+    12,
+    2,
+    1,
+    6,
+    13,
+    14 };
+            var return_list = new List<int>();
+            var input_dic = new Dictionary<int, bool>();
+            foreach(var item in array)
+            {
+                if (!input_dic.ContainsKey(item)) input_dic.Add(item, false);
+            }
+
+            List<int> item_list;
+
+            foreach (int item in array)
+            {
+                if (input_dic[item]) continue;
+                else
+                {
+                    item_list = new List<int>();
+                    input_dic[item] = true;
+                    item_list.Add(item);
+                    int i = 1;
+                    while (true)
+                    {
+                        if (input_dic.ContainsKey(item - i))
+                        {
+                            item_list.Add(item - i);
+                            input_dic[item - i] = true;
+                        }
+                        else break;
+
+                        i++;
+                    }
+
+                    i = 1;
+                    while (true)
+                    {
+                        if (input_dic.ContainsKey(item + i))
+                        {
+                            item_list.Add(item + i);
+                            input_dic[item + i] = true;
+                        }
+                        else break;
+
+                        i++;
+                    }
+
+
+                }
+
+                if(item_list.Count > return_list.Count)
+                {
+                    return_list = item_list;
+                }
+                
+                  
+            }
+
+            var return_array = new int[2] { return_list.Min(), return_list.Max() };
+
+             Console.WriteLine(return_list.ToArray());
+
+
+
+
         }
     }
 }
