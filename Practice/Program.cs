6582e4ce3ef0bf8bf6ad31a473ad3bb1136c8b53
olayemi odefunsho <odefunsho.olayemi@gmail.com>
2020-05-18 22:20:34 +0100

    Two number Sum

--- Practice/Program.cs
+++ Practice/Program.cs
@@ -6,91 +6,47 @@ namespace Practice
 {
     class Program
     {
+        public static int[] TwoNumberSum(int[] array, int targetSum)
+        {
+            int[] return_array = new int[2];
+            Array.Sort(array);
+
+            int x = 0;
+            int y = array.Length - 1;
+
+            while (x < y)
+            {
+                if (array[x] + array[y] == targetSum)
+                {
+                    return new int[] { array[x], array[y] };                   
+                }
+
+                if (array[x] + array[y] < targetSum)
+                {
+                    x++;
+                    continue;
+                }
+
+                if (array[x] + array[y] > targetSum)
+                {
+                    y--;
+                    continue;
+                }
+            }
+
+
+            return new int[0];
+        }
+
+
         static void Main(string[] args)
         {
-            int[] array = new int[] { 19, -1,
-    18,
-    17,
-    2,
-    10,
-    3,
-    12,
-    5,
-    16,
-    4,
-    11,
-    8,
-    7,
-    6,
-    15,
-    12,
-    12,
-    2,
-    1,
-    6,
-    13,
-    14 };
-            var return_list = new List<int>();
-            var input_dic = new Dictionary<int, bool>();
-            foreach(var item in array)
-            {
-                if (!input_dic.ContainsKey(item)) input_dic.Add(item, false);
-            }
-
-            List<int> item_list;
-
-            foreach (int item in array)
-            {
-                if (input_dic[item]) continue;
-                else
-                {
-                    item_list = new List<int>();
-                    input_dic[item] = true;
-                    item_list.Add(item);
-                    int i = 1;
-                    while (true)
-                    {
-                        if (input_dic.ContainsKey(item - i))
-                        {
-                            item_list.Add(item - i);
-                            input_dic[item - i] = true;
-                        }
-                        else break;
-
-                        i++;
-                    }
-
-                    i = 1;
-                    while (true)
-                    {
-                        if (input_dic.ContainsKey(item + i))
-                        {
-                            item_list.Add(item + i);
-                            input_dic[item + i] = true;
-                        }
-                        else break;
-
-                        i++;
-                    }
-
-
-                }
-
-                if(item_list.Count > return_list.Count)
-                {
-                    return_list = item_list;
-                }
-                
-                  
-            }
-
-            var return_array = new int[2] { return_list.Min(), return_list.Max() };
-
-             Console.WriteLine(return_list.ToArray());
-
-
-
-
+            int[] array = new int[] {-4,-1,1,3,5,6,8,11};
+            int targetSum = 13;
+            Console.WriteLine(TwoNumberSum(array, targetSum));
         }
+
+
+        
     }
 }
