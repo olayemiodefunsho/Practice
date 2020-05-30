43a8c43bae6de908dda24bc3a2f9a7b48609e398
olayemi odefunsho <odefunsho.olayemi@gmail.com>
2020-05-30 22:04:41 +0100

    Sub Array Sort

--- Practice/Program.cs
+++ Practice/Program.cs
@@ -6,68 +6,72 @@ namespace Practice
 {
     class Program
     {
-        public static List<int[]> FourNumberSum(int[] array, int targetSum)
+        public static int[] SubarraySort(int[] array)
         {
-            var return_list = new List<int[]>();
-            var holder_dic = new Dictionary<int, List<int[]>>();
-            int key, j;
-            List<int[]> int_list;
-            bool if_exist = false;
+            var unsorted_list = new List<int>();
+            bool not_last = false;
+            
 
-            for (int i = 1; i < array.Length - 1; i++)
-            {                             
-                for(j = i + 1; j <= array.Length - 1; j++)
+            for(int i = 1; i < array.Length - 1; i++)
+            {
+                if(!(array[i] >= array[i - 1] && array[i] <= array[i+1]))
                 {
-                    key = targetSum - (array[i] + array[j]);
-                    if (holder_dic.ContainsKey(key))
-                    {
-                        foreach(var item in holder_dic[key])
-                        {
-                            return_list.Add(new int[] { item[0], item[1],array[i], array[j]});
-                        }                      
-                    }                   
+                    unsorted_list.Add(i);
                 }
-
-                for(j = 0; j < i; j++)
-                {
-                    int_list = new List<int[]>();
-                    key = array[i] + array[j];
-                    if (!holder_dic.ContainsKey(key))
-                    {
-                        int_list.Add(new int[] { array[j], array[i] });
-                        holder_dic.Add(key, int_list);
-                    }
-                    else
-                    {
-                        foreach (var item in holder_dic[key])
-                        {
-                            if((array[i] == item[0] && array[j] == item[1]) || (array[j] == item[0] && array[i] == item[1]))
-                            {
-                                if_exist = true;
-                                break;
-                            }
-                        }
-
-                        if(!if_exist)
-                        {
-                            holder_dic[key].Add(new int[] { array[j], array[i] });
-                        }
-                    }
-
-                }
-
             }
-            // Write your code here.
-            return return_list;
+
+            if (array[array.Length - 1] < array[array.Length - 2]) unsorted_list.Add(array.Length - 1);
+
+            if (unsorted_list.Count == 0) return new int[] { -1,-1};
+
+            int s_index = unsorted_list[0];
+            int l_index = unsorted_list[0];
+
+            foreach (var item in unsorted_list)
+            {
+                if(array[item] > array[l_index])
+                {
+                    l_index = item;
+                }
+
+                if (array[item] < array[s_index])
+                {
+                    s_index = item;
+                }
+            }
+
+            for(int i = 0; i < array.Length; i++)
+            {
+                if(array[s_index] < array[i])
+                {
+                    s_index = i;
+                    break;
+                }
+            }
+
+            for(int i = s_index + 1; i < array.Length; i++)
+            {
+
+                if (array[l_index] < array[i])
+                {
+                    l_index = i - 1;
+                    not_last = true;
+                    break;
+                }
+            }
+
+            if (!not_last) l_index = array.Length - 1;
+
+
+            return new int[] { s_index, l_index };
         }
 
 
         static void Main(string[] args)
         {
-            int[] array = new int[] { 5, -5, -2, 2, 3, -3 };
-            int targetSum = 0;
+            int[] array = new int[] { 1,2 };
 
-            var output = FourNumberSum(array, targetSum);
+            var output = SubarraySort(array);
 
         }
 
