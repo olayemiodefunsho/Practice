5636a1020df4cbc223d2723fa385aab93eff22e9
olayemi odefunsho <odefunsho.olayemi@gmail.com>
2020-06-02 23:42:04 +0100

    Zigzag Traverse

--- Practice/Program.cs
+++ Practice/Program.cs
@@ -6,47 +6,92 @@ namespace Practice
 {
     class Program
     {
-        //MIN REWARDS - minimiun reward you can give to members of an array deoending on their score
-        //create an array with same size and fill with all 1s
-        //go thru scores from left to right
-        //For every element that is greater than the one before it
-        //update it's value in array with which ever is greater
-        //between it's value and the one before it plus 1
-        //now run from right to left
-        //For every element that is greater than the one before it
-        //update array with max just as above
-        //npw return the sum of all elements in array
-        public static int MinRewards(int[] scores)
+        //ZIG ZAG TRAVERSE - traverse a 2 dimensional array in a zigzag manner
+        //at every move check if you are at the edges
+        //if you are at the edge alternate the movement either up or down
+        //the edge you are at determines if you move down or sideways on the edge
+        //if you are not at an edge just keep moving in your current direction
+        public static List<int> ZigzagTraverse(List<List<int>> array)
         {
-            var array = new int[scores.Length];
-            Array.Fill(array, 1);
 
-            for(int i = 1; i < scores.Length; i++)
+            var return_list = new List<int>();
+            int x = 0;
+            int y = 0;
+            bool point_down = true;
+
+            while(x < array.Count && y < array[0].Count)
             {
-                if(scores[i] > scores[i - 1])
+                return_list.Add(array[x][y]);
+                if(point_down)
                 {
-                    array[i] = Math.Max(array[i], array[i - 1] + 1);
+                    if(y == 0)
+                    {
+                        x++;
+                        point_down = false;
+                        continue;
+                    }
+                    else
+                    {
+                        if (x != array.Count - 1)
+                        {
+                            x++;
+                            y--;
+                        }
+                        else
+                        {
+                            y++;
+                            point_down = false;
+                        }
+                        continue;
+                    }                   
                 }
-            }
-
-            for (int i = scores.Length - 2; i >= 0; i--)
-            {
-                if (scores[i] > scores[i + 1])
+                else
                 {
-                    array[i] = Math.Max(array[i], array[i + 1] + 1);
+                    if(x == 0)
+                    {
+                        if (y != array[0].Count - 1) y++;
+                        else x++;
+                        point_down = true;
+                        continue;
+                    }
+                    else
+                    {
+                        if(y != array[0].Count - 1)
+                        {
+                            x--;
+                            y++;
+                        }
+                        else
+                        {
+                            x--;
+                            point_down = true;
+                        }
+                       
+                        continue;
+                    }
+
                 }
+
+
+
             }
-
-
-            return array.Sum();
+            
+            
+            return null;
         }
 
 
         static void Main(string[] args)
         {
-            int[] scores = new int[] { 8,4,2,1,3,6,7,9,5};
+            var list1 = new List<int>() { 1, 3, 4, 10 };
+            var list2 = new List<int>() { 2, 5, 9, 11 };
+            var list3 = new List<int>() { 6, 8, 12, 15 };
+            var list4 = new List<int>() { 7, 13, 14, 16 };
 
-            var output = MinRewards(scores);
+            var array = new List<List<int>>() { list1, list2, list3, list4 };
+            
+
+            var output = ZigzagTraverse(array);
 
         }
 
