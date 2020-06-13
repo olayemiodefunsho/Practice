62b548b6f0bd4ec585397699bb10c5bf8397ccf4
olayemi odefunsho <odefunsho.olayemi@gmail.com>
2020-06-13 17:40:05 +0100

    zigzag traverse

--- Practice/Program.cs
+++ Practice/Program.cs
@@ -7,10 +7,10 @@ namespace Practice
     class Program
     {
         //ZIG ZAG TRAVERSE - traverse a 2 dimensional array in a zigzag manner
-        //at every move check if you are at the edges
-        //if you are at the edge alternate the movement either up or down
-        //the edge you are at determines if you move down or sideways on the edge
-        //if you are not at an edge just keep moving in your current direction
+        //when going up check if we are by the left or bottom perimeter
+        //when going down check if we ar at the right or top perimeter
+        //else just move diagnally depending on up or down minue or plus x and y as needed
+        // at the perimeters come down or go right till the end
         public static List<int> ZigzagTraverse(List<List<int>> array)
         {
 
@@ -20,75 +20,85 @@ namespace Practice
             bool point_down = true;
 
             while(x < array.Count && y < array[0].Count)
-            {
-                return_list.Add(array[x][y]);
+            {                
                 if(point_down)
                 {
-                    if(y == 0)
+                    if(x == array.Count - 1 || y == 0)
                     {
-                        x++;
-                        point_down = false;
-                        continue;
+                        if (y == 0 && x != array.Count - 1)
+                        {
+                            point_down = false;
+                            return_list.Add(array[x][y]);
+                            x++;
+                            continue;
+                        }
+                        if (x == array.Count - 1)
+                        {
+                            point_down = false;
+                            return_list.Add(array[x][y]);
+                            if (y != array[0].Count - 1) y++;
+                            else break;
+                            continue;
+                        }
                     }
                     else
                     {
-                        if (x != array.Count - 1)
-                        {
-                            x++;
-                            y--;
-                        }
-                        else
-                        {
-                            y++;
-                            point_down = false;
-                        }
-                        continue;
-                    }                   
+                        return_list.Add(array[x][y]);
+                        x++;
+                        y--;
+                    }                                      
                 }
                 else
                 {
-                    if(x == 0)
+                    if(x == 0 || y == array[0].Count - 1)
                     {
-                        if (y != array[0].Count - 1) y++;
-                        else x++;
-                        point_down = true;
-                        continue;
+                        if (x == 0 && y != array[0].Count - 1)
+                        {
+                            point_down = true;
+                            return_list.Add(array[x][y]);
+                            y++;
+                            continue;
+                        }                  
+                        if (y == array[0].Count - 1)
+                        {
+                            point_down = true;
+                            return_list.Add(array[x][y]);
+                            if (x != array.Count - 1) x++;
+                            else break;
+                            return_list.Add(array[x][y]);
+                            x++;
+                            if (y != 0) y--;                        
+                            continue;
+                        }
+
                     }
                     else
                     {
-                        if(y != array[0].Count - 1)
-                        {
-                            x--;
-                            y++;
-                        }
-                        else
-                        {
-                            x--;
-                            point_down = true;
-                        }
-                       
-                        continue;
-                    }
-
+                        return_list.Add(array[x][y]);
+                        x--;
+                        y++;
+                    }                    
+                                       
                 }
-
-
-
-            }
-            
-            
-            return null;
+            }            
+            return return_list;
         }
 
 
         static void Main(string[] args)
         {
-            var list1 = new List<int>() { 1, 3, 4, 10 };
-            var list2 = new List<int>() { 2, 5, 9, 11 };
-            var list3 = new List<int>() { 6, 8, 12, 15 };
-            var list4 = new List<int>() { 7, 13, 14, 16 };
+            var list1 = new List<int>() {1};
+            //var list2 = new List<int>() {2};
+            //var list3 = new List<int>() {3};
+            //var list4 = new List<int>() {4};
+            //var list5 = new List<int>() {5};
 
-            var array = new List<List<int>>() { list1, list2, list3, list4 };
+            //var list1 = new List<int>() { 1, 3, 4, 10 };
+            //var list2 = new List<int>() { 2, 5, 9, 11 };
+            //var list3 = new List<int>() { 6, 8, 12, 15 };
+            //var list4 = new List<int>() { 7, 13, 14, 16 };
+
+            var array = new List<List<int>>() { list1};
             
 
             var output = ZigzagTraverse(array);
