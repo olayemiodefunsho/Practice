4e7420ba67956c6a138fc5564ab7764592523e56
olayemi odefunsho <odefunsho.olayemi@gmail.com>
2020-06-14 22:15:28 +0100

    Nth Fibonacci

--- Practice/Program.cs
+++ Practice/Program.cs
@@ -6,102 +6,37 @@ namespace Practice
 {
     class Program
     {
-        //ZIG ZAG TRAVERSE - traverse a 2 dimensional array in a zigzag manner
-        //when going up check if we are by the left or bottom perimeter
-        //when going down check if we ar at the right or top perimeter
-        //else just move diagnally depending on up or down minue or plus x and y as needed
-        // at the perimeters come down or go right till the end
-        public static List<int> ZigzagTraverse(List<List<int>> array)
+        //NTH FIBONACCI
+        //if n is 1 return 0 and if n is 2 return 1
+        //Initialize 2 dimensinal array with 0 and 1
+        //add the 2 numbers to get the next fib number
+        //remove index 0, move index 1 to index 0 and put the new fibonacci number in index 1
+        //repart the same for the number of ns
+        public static int GetNthFib(int n)
         {
+            if(n == 1) return 0;
+            if (n == 2) return 1;
 
-            var return_list = new List<int>();
-            int x = 0;
-            int y = 0;
-            bool point_down = true;
+            var array = new int[] { 0, 1 };
 
-            while(x < array.Count && y < array[0].Count)
-            {                
-                if(point_down)
-                {
-                    if(x == array.Count - 1 || y == 0)
-                    {
-                        if (y == 0 && x != array.Count - 1)
-                        {
-                            point_down = false;
-                            return_list.Add(array[x][y]);
-                            x++;
-                            continue;
-                        }
-                        if (x == array.Count - 1)
-                        {
-                            point_down = false;
-                            return_list.Add(array[x][y]);
-                            if (y != array[0].Count - 1) y++;
-                            else break;
-                            continue;
-                        }
-                    }
-                    else
-                    {
-                        return_list.Add(array[x][y]);
-                        x++;
-                        y--;
-                    }                                      
-                }
-                else
-                {
-                    if(x == 0 || y == array[0].Count - 1)
-                    {
-                        if (x == 0 && y != array[0].Count - 1)
-                        {
-                            point_down = true;
-                            return_list.Add(array[x][y]);
-                            y++;
-                            continue;
-                        }                  
-                        if (y == array[0].Count - 1)
-                        {
-                            point_down = true;
-                            return_list.Add(array[x][y]);
-                            if (x != array.Count - 1) x++;
-                            else break;
-                            return_list.Add(array[x][y]);
-                            x++;
-                            if (y != 0) y--;                        
-                            continue;
-                        }
+            int i = 2;
+            int sum = 0;
+            while(i < n)
+            {
+                sum = array[0] + array[1];
+                array[0] = array[1];
+                array[1] = sum;
+                i++;
+            }
 
-                    }
-                    else
-                    {
-                        return_list.Add(array[x][y]);
-                        x--;
-                        y++;
-                    }                    
-                                       
-                }
-            }            
-            return return_list;
+            return array[1];
         }
 
 
         static void Main(string[] args)
         {
-            var list1 = new List<int>() {1};
-            //var list2 = new List<int>() {2};
-            //var list3 = new List<int>() {3};
-            //var list4 = new List<int>() {4};
-            //var list5 = new List<int>() {5};
-
-            //var list1 = new List<int>() { 1, 3, 4, 10 };
-            //var list2 = new List<int>() { 2, 5, 9, 11 };
-            //var list3 = new List<int>() { 6, 8, 12, 15 };
-            //var list4 = new List<int>() { 7, 13, 14, 16 };
-
-            var array = new List<List<int>>() { list1};
-            
-
-            var output = ZigzagTraverse(array);
+            int n = 6;            
+            var output = GetNthFib(n);
 
         }
 
