ce95ed1d7bcea691cdf79f0989697b70c59ccd98
olayemi odefunsho <odefunsho.olayemi@gmail.com>
2020-06-14 23:19:17 +0100

    Palindrome

--- Practice/Program.cs
+++ Practice/Program.cs
@@ -6,40 +6,34 @@ namespace Practice
 {
     class Program
     {
-        //NTH FIBONACCI
-        //if n is 1 return 0 and if n is 2 return 1
-        //Initialize 2 dimensinal array with 0 and 1
-        //add the 2 numbers to get the next fib number
-        //remove index 0, move index 1 to index 0 and put the new fibonacci number in index 1
-        //repart the same for the number of ns
-        public static int GetNthFib(int n)
+        //String  - Palindrome check
+        //put a pointer on the first and last string
+        //keep comparong both and moving closer till thy meet
+        //if at any point they differ fail and return false
+        //if it gets to the end return true, that's a palindrome
+        public static bool IsPalindrome(string str)
         {
-            if(n == 1) return 0;
-            if (n == 2) return 1;
-
-            var array = new int[] { 0, 1 };
-
-            int i = 2;
-            int sum = 0;
-            while(i < n)
+            bool return_value = true;
+            int i = 0;
+            int j = str.Length - 1;
+            while(i < j)
             {
-                sum = array[0] + array[1];
-                array[0] = array[1];
-                array[1] = sum;
+                if (str[i] != str[j]) return false;
                 i++;
+                j--;
             }
 
-            return array[1];
+            return return_value;
         }
 
 
         static void Main(string[] args)
         {
-            int n = 6;            
-            var output = GetNthFib(n);
+            string str = "abcdcba";
+            var output = IsPalindrome(str);
+            
 
         }
-
 
 
     }
