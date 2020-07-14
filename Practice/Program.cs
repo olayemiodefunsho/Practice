392d7e15e99541e022fbb38dfda4013d3e8135ff
olayemi odefunsho <odefunsho.olayemi@gmail.com>
2020-07-14 23:16:04 +0100

    Sorting - insertion Sort

--- Practice/Program.cs
+++ Practice/Program.cs
@@ -7,26 +7,33 @@ namespace Practice
 {
     class Program
     {
-        public static int BinarySearch(int[] array, int target)
+        public static int[] InsertionSort(int[] array)
         {
-            int l = 0, m = 0;
-            int r = array.Length - 1;
-            while(l <= r)
-            {
-                m = (l + r) / 2;
-                if (array[m] == target) return m;
+            int j = 0;
+            int holder = 0;
 
-                if (array[m] > target) r = m - 1;
-                else l = m + 1;
-            }
-            return -1;
+           for(int i = 0; i < array.Length; i++)
+           {
+                j = i;
+                while(j > 0)
+                {
+                    if (array[j] < array[j - 1])
+                    {
+                        holder = array[j];
+                        array[j] = array[j - 1];
+                        array[j - 1] = holder;
+                        j--;
+                    }
+                    else break;
+                }
+           }
+            return array;
         }
 
         static void Main(string[] args)
         {
-            var array = new int[] { 0, 1, 21, 33, 45, 45, 61, 71, 72, 73 };
-            var target = 70;
-            var output = BinarySearch(array, target);           
+            var array = new int[] { 3, 2, 5, 5, 6, 8, 9 };
+            var output = InsertionSort(array);           
         }
 
 
