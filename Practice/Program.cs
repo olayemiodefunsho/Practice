f8cd4390c42c9d368dfa13f585c08de173aa2d67
olayemi odefunsho <odefunsho.olayemi@gmail.com>
2020-07-18 00:36:43 +0100

    Searching - Shifted Binary Search

--- Practice/Program.cs
+++ Practice/Program.cs
@@ -7,45 +7,34 @@ namespace Practice
 {
     class Program
     {
-        public static int[] SearchInSortedMatrix(int[,] matrix, int target)
+        public static int ShiftedBinarySearch(int[] array, int target)
         {
-            int x = 0, y = matrix.GetLength(0), maxy = y, maxx = matrix.GetUpperBound(0);
-            while((x <= maxx && x >= 0) && (y <= maxy && y >= 0))
+            int l = 0, r = array.Length - 1, m = 0;
+
+            while(l <= r)
             {
-                if(matrix[x,y] == target)
+                m = (l + r) / 2;
+                if (array[m] == target) return m;
+                if(array[l] <= array[m])
                 {
-                    return new int[] { x, y };
+                    if(target >= array[l] && target <= array[m]) r = m - 1;
+                    else l = m + 1;
                 }
-
-                if(target > matrix[x, y])
+                else
                 {
-                    x++;
-                    continue;
-                }
-
-                if(target < matrix[x, y])
-                {
-                    y--;
-                    continue;
+                    if (target >= array[m] && target <= array[r]) l = m + 1;
+                    else r = m - 1;
                 }
             }
 
-            return new int[] { -1, -1 };
+            return -1;
         }
 
         static void Main(string[] args)
-        {            
-            var array = new int[,]
-            {
-               { 1, 4, 7, 12, 15, 1000 },
-               { 2, 5, 19, 31, 32, 1001 },
-               { 3, 8, 24, 33, 35, 1002 },
-               { 40, 41, 42, 44, 45, 1003 },
-               { 99, 100, 103, 106, 128, 1004 }
-
-            };
-            int target = 43;
-            var output = SearchInSortedMatrix(array, target);           
+        {
+            var array = new int[] { 72, 73, 0, 1, 21, 33, 37, 45, 61, 71 };
+            int target = 72;
+            var output = ShiftedBinarySearch(array, target);           
         }
     }
 }
